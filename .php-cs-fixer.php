fba17873d0b859f5d25e4604923ade24d0adf2ad
Francesco Loreti <francesco.loreti@isprambiente.it>
2026-03-20 10:32:14 +0100

    Configurate librerie per i test del codice

---
+++ .php-cs-fixer.php
@@ -0,0 +1,12 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(['src', 'tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
